5f36ab02cb93b299a06906022d25b44218a80de5
pierofabiani <pierofabiani91@gmail.com>
2022-09-13 17:13:41 +0200

    hiding env vars

--- cv_website/cv_website/settings.py
+++ cv_website/cv_website/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
+
 from pathlib import Path
+
+# Load dotenv for env variables
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-k7@#1k8rq47@85hj-o(c+_47)d$u$q6*@gs%!52$zwgff(=d3('
+SECRET_KEY = os.environ["DJANGO_SECRET"]
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
