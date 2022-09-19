a026ee718e126f2b571b71e2c19754610c196131
Guido Lippi <guidolippi94@gmail.com>
2022-09-19 15:20:45 +0200

    created models cvelement

--- portfolio_django/portfolio/models.py
+++ portfolio_django/portfolio/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+
+class cvelement(models.Model):
+    nome = models.CharField(max_length=20)
+    descrizione = models.CharField(max_length=60)
+    riassunto = models.CharField(max_length=500)
+    start = models.DateField()
+    end = models.DateField()
