834600c912fce347d683f941649b38d0627bf50e
Guido Lippi <guidolippi94@gmail.com>
2022-10-03 14:43:18 +0200

    Added contacts view

--- portfolio_django/portfolio/views.py
+++ portfolio_django/portfolio/views.py
@@ -5,3 +5,8 @@ from .models import cvelement
 def cvsite(request):
     cvelements = cvelement.objects.all()
     return render(request, 'portfolio/homepage.html', {'cvelements': cvelements})
+
+
+def contatti(request):
+    cvelements = cvelement.objects.all()
+    return render(request, 'portfolio/contatti.html', {'cvelements': cvelements})
--- portfolio_django/portfolio_django/urls.py
+++ portfolio_django/portfolio_django/urls.py
@@ -19,5 +19,6 @@ from portfolio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.cvsite)
+    path('', views.cvsite, name="home"),
+    path('contatti', views.contatti, name="contatti")
 ]
